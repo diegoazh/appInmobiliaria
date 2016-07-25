f37e175398bee833daafdc65dc4b6262a2ca5f05
diegoazh <diegoazh2003@gmail.com>
2016-07-25 09:23:04 -0300

    Realizamos actualizaciones en la parte de comentarios finalizados.

--- AppTP/Controllers/ComentariosController.cs
+++ AppTP/Controllers/ComentariosController.cs
@@ -54,7 +54,15 @@ namespace AppTP.Controllers
             List<object> comment = new List<object>();
             var comentario = db.Comentario.Single(x => x.id_comentario == id_comentario);
             Comentario c = new Comentario();
+            c.nombre = comentario.nombre;
+            c.apellido = comentario.apellido;
+            c.mail = comentario.mail;
+            c.telefono = comentario.telefono;
+            c.celular = comentario.celular;
+            c.fecha_pregunta = comentario.fecha_pregunta;
             c.comentario1 = comentario.comentario1.Replace(Environment.NewLine, "*");
+            c.fecha_respuesta = comentario.fecha_respuesta;
+            c.respuesta = comentario.respuesta;
             comment.Add(c);
             var json = JsonConvert.SerializeObject(comment);
             return Json(json, JsonRequestBehavior.AllowGet);
@@ -64,8 +72,12 @@ namespace AppTP.Controllers
         public JsonResult cerrar_comentario(int id_comentario, string respuesta, int id_usuario)
         {
             var com = db.Comentario.Single(x => x.id_comentario == id_comentario);
+            var user = db.Usuario.Single(x => x.id_usuario == id_usuario);
             com.fecha_respuesta = DateTime.UtcNow;
-            com.respuesta = respuesta;
+            if (String.IsNullOrEmpty(com.respuesta))
+                com.respuesta = respuesta + "\n(" + user.username + " - " + DateTime.UtcNow + ")";
+            else
+                com.respuesta += "\n" + respuesta + "\n(" + user.username + " - " + DateTime.UtcNow + ")";
             com.id_usuario = id_usuario;
             db.SubmitChanges();
             var json = JsonConvert.SerializeObject(new JsonResult { Data = "El comentario se finalizó de manera correcta" });
--- AppTP/Scripts/comentarios.js
+++ AppTP/Scripts/comentarios.js
@@ -1,40 +1,51 @@
 ﻿$(document).ready(function () {
     'use strict'
+    if (window.location.pathname === '/Comentarios') {
+        alertToogle();
+        caracteresTextarea('respuesta', 'cant_char');
 
-    alertToogle();
-    caracteresTextarea('respuesta', 'cant_char');
-
-    if (leerCookie('cerrado')) {
-        $('#alert_backend_usuario').removeClass('hidden').addClass('alert-success');
-        $('#texto_alert_usuarios').html(leerCookie('cerrado'));
-        eliminarCookie('cerrado');
-    }
-
-    $('.cerrar-comentario').on('click', function (event) {
-        event.preventDefault();
-        var id = $(this).attr('id');
-        id = id.split('_');
-        id = id[id.length - 1];
-        $('#modal_cerrar_comentario').modal('show');
-        $('#id_comentario').val(id);
-        $('#id_usuario').val(leerCookie('UserId'));
-        $('.cerrar-comentario-tt').html('<i class="fa fa-commenting-o" aria-hidden="true"></i> ' + $('#nombre_' + id).text() + ' <small><i class="fa fa-envelope-square" aria-hidden="true"></i> ' + $('#mail_' + id).text() + ' | <i class="fa fa-calendar" aria-hidden="true"></i> ' + $('#fecha_' + id).text() + '</small>');
-        
-        function setearContenidoComentario(data) {
-            var comment = JSON.parse(data);
-            var text = comment[0].comentario1.split('*');
-            text = text.join('</p><p>');
-            $('#contenido_comentario').html(text);
+        if (leerCookie('cerrado')) {
+            $('#alert_backend_usuario').removeClass('hidden').addClass('alert-success');
+            $('#texto_alert_usuarios').html(leerCookie('cerrado'));
+            eliminarCookie('cerrado');
         }
-        consultaAjax('/Comentarios/buscar_comentario', 'POST', 'json', { tabla: 'Comentario', id_comentario: parseInt(id) }, false, 'application/x-www-form-urlencoded; charset=UTF-8', true, setearContenidoComentario);
-        $('#btn_cerrar_comentario').on('click', function () {
-            function mensajeServidor(data) {
-                var response = JSON.parse(data);
-                crearCookie('cerrado', response.Data);
-                window.location.href = 'http://' + window.location.host + '/Comentarios';
+
+        $('.cerrar-comentario').on('click', function (event) {
+            event.preventDefault();
+            var id = $(this).attr('id');
+            id = id.split('_');
+            id = id[id.length - 1];
+            $('#modal_cerrar_comentario').modal('show');
+            $('#id_comentario').val(id);
+            $('#id_usuario').val(leerCookie('UserId'));
+            $('.cerrar-comentario-tt').html('<i class="fa fa-commenting-o" aria-hidden="true"></i> ' + $('#nombre_' + id).text() + ' <small><i class="fa fa-envelope-square" aria-hidden="true"></i> ' + $('#mail_' + id).text() + ' | <i class="fa fa-calendar" aria-hidden="true"></i> ' + $('#fecha_' + id).text() + '</small>');
+        
+            function setearContenidoComentario(data) {
+                var comment = JSON.parse(data);
+                var text = comment[0].comentario1.split('*');
+                text = text.join('</p><p>');
+                $('#contenido_comentario').html(text);
             }
-            consultaAjax('/Comentarios/cerrar_comentario', 'POST', 'json', { id_comentario: $('#id_comentario').val(), respuesta: $('#respuesta').val(), id_usuario: $('#id_usuario').val() }, false, 'application/x-www-form-urlencoded; charset=UTF-8', true, mensajeServidor);
-            $('#modal_cerrar_comentario').modal('hide');
+            consultaAjax('/Comentarios/buscar_comentario', 'POST', 'json', { tabla: 'Comentario', id_comentario: parseInt(id) }, false, 'application/x-www-form-urlencoded; charset=UTF-8', true, setearContenidoComentario);
+            $('#btn_cerrar_comentario').on('click', function () {
+                function mensajeServidor(data) {
+                    var response = JSON.parse(data);
+                    crearCookie('cerrado', response.Data);
+                    window.location.href = 'http://' + window.location.host + '/Comentarios';
+                }
+                consultaAjax('/Comentarios/cerrar_comentario', 'POST', 'json', { id_comentario: $('#id_comentario').val(), respuesta: $('#respuesta').val(), id_usuario: $('#id_usuario').val() }, false, 'application/x-www-form-urlencoded; charset=UTF-8', true, mensajeServidor);
+                $('#modal_cerrar_comentario').modal('hide');
+            });
         });
-    });
+    } else if (window.location.pathname === "/Comentarios/Finalizados") {
+        $('.ver-cerrado').click(function (event) {
+            event.preventDefault();
+            $('#ver_cerrados').modal('show');
+        });
+
+        $('.eliminar-cerrado').click(function (event) {
+            event.preventDefault();
+            $('#confirmar_eliminar').modal('show');
+        });
+    }
 });

a81cf0423c95174a2c9b3ff01545176305cc120f
diegoazh <diegoazh2003@gmail.com>
2016-08-01 15:57:03 -0300

    Valoracion a medias

---
+++ AppTP/Controllers/ValoracionController.cs
@@ -0,0 +1,34 @@
+﻿using AppTP.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppTP.Controllers
+{
+    public class ValoracionController : Controller
+    {
+        private ElTrebolDBDataContext db = new ElTrebolDBDataContext();
+
+        [HttpPost, AllowAnonymous]
+        public ActionResult votar(int estrellas, int id_publicacion, string controller)
+        {
+            Publicacion publi = db.Publicacion.Single(x => x.id_publicacion == id_publicacion);
+
+            int? votantes = publi.votantes == null ? 0 : publi.votantes;
+            decimal? valoracion = publi.valoracion == null ? 0 : publi.valoracion;
+            decimal? total = votantes * valoracion;
+            total += estrellas;
+            votantes++;
+            valoracion = total / votantes;
+            publi.valoracion = valoracion;
+            publi.votantes = votantes;
+            db.SubmitChanges();
+
+            return RedirectToAction("Index", controller);
+        }
+
+    }
+}
---
+++ AppTP/Scripts/valoracion.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function () {
+    'use strict';
+
+    $('#btn_valoracion').on('mouseover', function () {
+        $('i.fa-star-o').mouseover(function () {
+            $(this).addClass('fondo-amarillo');
+            $(this).click(function () {
+                var voto = $(this).attr('id');
+                voto = voto.split('_');
+                voto = voto[voto.length - 1];
+                var idPubli = $('#id_publicacion').text();
+                var controller = window.location.pathname;
+                controller = controller.split('/');
+                controller = controller[0];
+                function callback(data) {
+                    var dt = JSON.parse(data);
+                    $('#alert_producto').removeClass('hidden').addClass('alert-success');
+                    $('#texto_alert_product').html(dt.Data);
+                }
+                consultaAjax('/Valoracion/votar', 'POST', 'json', { estrellas: voto, id_publicacion: idPubli, controller: controller }, false, 'application/x-www-form-urlencoded; charset=UTF-8', true, callback);
+            });
+        });
+        $('i.fa-star-o').mouseout(function () {
+            $(this).removeClass('fondo-amarillo');
+            $(this).off('click');
+        });
+    });
+});
